74b3cf01aa418b7c9f71c7f31e1602961333ec1e
antskat <donaldsmak7@gmail.com>
2023-09-25 21:53:50 +0300

    Почти закончил хтмл главной страницы и добавил много цсс

---
+++ js/contacts.js
@@ -0,0 +1,89 @@
+
+function changeInstaLogo1() {
+    document.getElementById('ginstlogo1').style.display = 'flex';
+    document.getElementById('winstlogo1').style.display = 'flex';
+}
+
+function rechangeInstaLogo1() {
+    document.getElementById('ginstlogo1').style.display = 'flex';
+    document.getElementById('winstlogo1').style.display = 'none';
+}
+
+
+function changeInstaLogo2() {
+    document.getElementById('ginstlogo2').style.display = 'flex';
+    document.getElementById('winstlogo2').style.display = 'flex';
+}
+
+function rechangeInstaLogo2() {
+    document.getElementById('ginstlogo2').style.display = 'flex';
+    document.getElementById('winstlogo2').style.display = 'none';
+}
+
+
+function changeInstaLogo3() {
+    document.getElementById('ginstlogo3').style.display = 'flex';
+    document.getElementById('winstlogo3').style.display = 'flex';
+}
+
+function rechangeInstaLogo3() {
+    document.getElementById('ginstlogo3').style.display = 'flex';
+    document.getElementById('winstlogo3').style.display = 'none';
+}
+
+
+function changeInstaLogo4() {
+    document.getElementById('ginstlogo4').style.display = 'flex';
+    document.getElementById('winstlogo4').style.display = 'flex';
+}
+
+function rechangeInstaLogo4() {
+    document.getElementById('ginstlogo4').style.display = 'flex';
+    document.getElementById('winstlogo4').style.display = 'none';
+}
+
+
+
+
+function changeTwitLogo1() {
+    document.getElementById('gtwitlogo1').style.display = 'flex';
+    document.getElementById('wtwitlogo1').style.display = 'flex';
+}
+
+function rechangeTwitLogo1() {
+    document.getElementById('gtwitlogo1').style.display = 'flex';
+    document.getElementById('wtwitlogo1').style.display = 'none';
+}
+
+
+function changeTwitLogo2() {
+    document.getElementById('gtwitlogo2').style.display = 'flex';
+    document.getElementById('wtwitlogo2').style.display = 'flex';
+}
+
+function rechangeTwitLogo2() {
+    document.getElementById('gtwitlogo2').style.display = 'flex';
+    document.getElementById('wtwitlogo2').style.display = 'none';
+}
+
+
+function changeTwitLogo3() {
+    document.getElementById('gtwitlogo3').style.display = 'flex';
+    document.getElementById('wtwitlogo3').style.display = 'flex';
+}
+
+function rechangeTwitLogo3() {
+    document.getElementById('gtwitlogo3').style.display = 'flex';
+    document.getElementById('wtwitlogo3').style.display = 'none';
+}
+
+
+function changeTwitLogo4() {
+    document.getElementById('gtwitlogo4').style.display = 'flex';
+    document.getElementById('wtwitlogo4').style.display = 'flex';
+}
+
+function rechangeTwitLogo4() {
+    document.getElementById('gtwitlogo4').style.display = 'flex';
+    document.getElementById('wtwitlogo4').style.display = 'none';
+}
--- js/index.js
+++
@@ -1,6 +0,0 @@
-// var btn1 = document.getElementById('inst4-btn');
-
-// btn1.addEventListener('onmouseover', function() {
-//     document.getElementById('inst4').style.display='none';
-//     document.getElementById('inst4_1').style.display='none';
-// });
