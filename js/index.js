a94ad0c655c0095176fcf3b8321ee9e3eeca4091
antskat <donaldsmak7@gmail.com>
2023-09-25 16:53:34 +0300

    s

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// var btn1 = document.getElementById('inst4-btn');
+
+// btn1.addEventListener('onmouseover', function() {
+//     document.getElementById('inst4').style.display='none';
+//     document.getElementById('inst4_1').style.display='none';
+// });
